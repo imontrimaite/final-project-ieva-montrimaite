51232992b9a1c59eb402cd2703b337513f9c4205
imontrimaite <montrimaite.ieva@gmail.com>
2023-02-19 18:33:11 +0200

    Implement Driver class

--- FrameworkProject/Driver.cs
+++ FrameworkProject/Driver.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace FrameworkProject
 {
-    internal class Driver
+    public class Driver
     {
+        private static IWebDriver driver;
+        public static void SetupDriver()
+        {
+            driver = new ChromeDriver();
+        }
+        public static IWebDriver GetDriver()
+        {
+            return driver;
+        }
+        public static void OpenURL(string url)
+        {
+            driver.Url = url;
+        }
+        public static void QuitDriver()
+        {
+            driver.Quit();
+        }
     }
 }
